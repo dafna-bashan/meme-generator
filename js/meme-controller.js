0ed81c2ba01043e0bcbb94904de360dc78255d25
Dafna Bashan <dafnaba1@gmail.com>
2021-04-19 21:58:04 +0300

    first commit

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,3 +1,129 @@
 'use strict';
 
-console.log('hi');
+var gCanvas;
+var gCtx;
+
+function onInit() {
+    gCanvas = document.getElementById('my-canvas');
+    gCtx = gCanvas.getContext('2d');
+    renderMeme();
+}
+
+function renderMeme() {
+    var meme = getMeme();
+    var lines = meme.lines;
+    var img = new Image()
+    img.src = `meme-imgs-square/${gMeme.selectedImgId}.jpg`;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        lines.forEach(function(line) {
+            drawText(line.txt, line.pos.x, line.pos.y, line.size, line.align, line.fillColor, line.strokeColor, line.fontFamily);
+            // console.log(gCtx.measureText(line.txt));
+            if (lines.indexOf(line) === meme.selectedLineIdx) {
+                drawBoundingBox(line);
+            }
+        })
+    }
+}
+
+
+function renderButtons() {
+    var meme = getMeme();
+    document.querySelector('input[name=text]').value = meme.lines[meme.selectedLineIdx].txt;
+    document.querySelector('select[name=font]').value = meme.lines[meme.selectedLineIdx].fontFamily;
+    document.querySelector('input[name=stroke-color]').value = meme.lines[meme.selectedLineIdx].strokeColor;
+    document.querySelector('input[name=fill-color]').value = meme.lines[meme.selectedLineIdx].fillColor;
+}
+
+function drawText(text, x, y, size, align, fillColor, strokeColor, fontFamily) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = strokeColor;
+    gCtx.fillStyle = fillColor;
+    gCtx.font = `${size}px ${fontFamily}`;
+    gCtx.textAlign = align;
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+function drawBoundingBox(currLine) {
+    var measures = gCtx.measureText(currLine.txt);
+    const height = 2 * (measures.actualBoundingBoxAscent + measures.actualBoundingBoxDescent);
+    const width = measures.actualBoundingBoxRight + measures.actualBoundingBoxLeft + 40;
+    const x = currLine.pos.x - (width / 2);
+    const y = currLine.pos.y - (height / 1.4);
+    gCtx.lineWidth = 1;
+    gCtx.beginPath()
+    gCtx.rect(x, y, width, height)
+    gCtx.fillStyle = 'transparent'
+    gCtx.fillRect(x, y, width, height)
+    gCtx.strokeStyle = 'black'
+    gCtx.stroke()
+}
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+    // You may clear part of the canvas
+    // gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height/4)
+}
+
+function onChangeLinePos(btnAction) {
+    changeLinePos(btnAction);
+    renderMeme();
+}
+
+function onUpdateImg(imgId) {
+    updateImg(imgId);
+    renderMeme();
+}
+
+function onUpdateText(elText) {
+    updateText(elText);
+    renderMeme();
+}
+
+function onChangeTextSize(btnAction) {
+    changeTextSize(btnAction);
+    renderMeme();
+}
+
+function onSwitchLines() {
+    switchLines();
+    renderButtons();
+    renderMeme();
+}
+
+function onSetFont(fontFamily) {
+    gCtx.font = "40px " + fontFamily;
+    gCtx.fillText("text", 0, 8);
+    setFont(fontFamily);
+    renderMeme();
+}
+
+function onSetStrokeColor(color) {
+    setStrokeColor(color);
+    renderMeme();
+}
+
+function onSetFillColor(color) {
+    setFillColor(color);
+    renderMeme();
+}
+
+function onSetAlignText(alignTo) {
+    setAlignText(alignTo);
+    renderMeme();
+}
+// function _loadFont(fontname) {
+//     var canvas = document.createElement("canvas");
+//     //Setting the height and width is not really required
+//     canvas.width = 16;
+//     canvas.height = 16;
+//     var ctx = canvas.getContext("2d");
+
+//     //There is no need to attach the canvas anywhere,
+//     //calling fillText is enough to make the browser load the active font
+
+//     //If you have more than one custom font, you can just draw all of them here
+//     ctx.font = "4px " + fontname;
+//     ctx.fillText("text", 0, 8);
+// }
--- js/meme-service.js
+++ js/meme-service.js
@@ -3,17 +3,85 @@
 var gKeywords = { 'happy': 12, 'funny puk': 1 }
 var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
 
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
-};
+// var gMeme = {
+//     selectedImgId: 5,
+//     selectedLineIdx: 0,
+//     lines: [{ txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }]
+// };
 
+var gMeme = createMeme(5);
 
 function createMeme(selectedImgId) {
     return {
         selectedImgId,
         selectedLineIdx: 0,
-        lines: [{ txt: 'edit your text', size: 20, align: 'center', color: 'black' }]
+        lines: [
+            { txt: 'edit your text', size: 40, align: 'center', fillColor: 'white', strokeColor: 'black', fontFamily: 'impact', pos: { x: 200, y: 80 } },
+            { txt: 'edit your text', size: 40, align: 'center', fillColor: 'white', strokeColor: 'black', fontFamily: 'impact', pos: { x: 200, y: 380 } }
+        ]
     };
+}
+
+function getMeme() {
+    return gMeme;
+}
+
+function updateText(text) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = text;
+}
+
+function updateImg(imgId) {
+    gMeme.selectedImgId = imgId;
+}
+
+function changeTextSize(btnAction) {
+    if (btnAction === 'decrease') {
+        gMeme.lines[gMeme.selectedLineIdx].size--;
+    } else {
+        gMeme.lines[gMeme.selectedLineIdx].size++;
+    }
+}
+
+function changeLinePos(btnAction) {
+    if (btnAction === 'down') {
+        gMeme.lines[gMeme.selectedLineIdx].pos.y++;
+    } else {
+        gMeme.lines[gMeme.selectedLineIdx].pos.y--;
+    }
+}
+
+function switchLines() {
+    if (gMeme.lines.length - 1 === gMeme.selectedLineIdx) {
+        gMeme.selectedLineIdx = 0;
+    } else {
+        gMeme.selectedLineIdx++;
+    }
+}
+
+function setFont(fontFamily) {
+    gMeme.lines[gMeme.selectedLineIdx].fontFamily = fontFamily;
+}
+
+function setStrokeColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].strokeColor = color;
+}
+
+function setFillColor(color) {
+    gMeme.lines[gMeme.selectedLineIdx].fillColor = color;
+}
+
+function setAlignText(alignTo) {
+    switch (alignTo) {
+        case 'left':
+            gMeme.lines[gMeme.selectedLineIdx].align = 'left';
+            break;
+        case 'center':
+            gMeme.lines[gMeme.selectedLineIdx].align = 'center';
+            break;
+        case 'right':
+            gMeme.lines[gMeme.selectedLineIdx].align = 'right';
+            break;
+        default:
+            break;
+    }
 }
